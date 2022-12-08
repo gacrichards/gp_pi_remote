faaaeb4a7aa6a192631c55d60225a185d052b34c
Doug Melton <dmelton@gopro.com>
2022-12-08 12:09:33 -0800

    Added gopro functionality to gp-remote.py

--- gp-remote.py
+++ gp-remote.py
@@ -1,33 +1,82 @@
 import RPi.GPIO as GPIO
+import sys
+from open_gopro import GoPro, Params
 from time import sleep
-SHUTTER = 24
-HILIGHT = 23
-IS_RECORDING = False
+SHUTTER_PIN = 24
+HILIGHT_PIN = 23
+is_encoding = False
 READY_FOR_HILIGHT = True
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SHUTTER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(HILIGHT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-print("Starting program")
-try:
-    while True:
-        if GPIO.input(SHUTTER):
-            if IS_RECORDING:
-                print("Recording in progress")
-                if GPIO.input(HILIGHT):
-                    if READY_FOR_HILIGHT:
-                        print("send hilight event")
-                        READY_FOR_HILIGHT = False
+
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SHUTTER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(HILIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def start_recording():
+    gopro.ble_command.set_shutter(Params.Toggle.ENABLE)
+
+def stop_recording():
+    gopro.ble_command.set_shutter(Params.Toggle.DISABLE)
+
+def tag_hilight():
+    gopro.ble_command.tag_hilight()
+
+def connect_gopro():
+    global gopro
+    gopro = GoPro(enable_wifi = False)
+    gopro.open()
+
+def init_gopro_settings():
+    if gopro.is_encoding:
+        stop_recording()
+    gopro.ble_setting.video_performance_mode.set(Params.PerformanceMode.MAX_PERFORMANCE)
+    gopro.ble_setting.max_lens_mode.set(Params.MaxLensMode.DEFAULT)
+    gopro.ble_setting.camera_ux_mode.set(Params.CameraUxMode.PRO)
+    gopro.ble_command.set_turbo_mode(False)
+    assert gopro.ble_command.load_preset_group(Params.PresetGroup.VIDEO).is_ok
+
+def main():
+    print("Starting program")
+
+    setup_gpio()
+    print("GPIO configured")
+
+    print("Connecting GoPro...")
+    connect_gopro()
+    print("-> Connected")
+
+    print("Initializing GoPro settings...")
+    init_gopro_settings()
+    print("-> Initialized")
+
+    try:
+        while True:
+            is_encoding = gopro.is_encoding
+            print(f"is encoding = {is_encoding}")
+            if GPIO.input(SHUTTER_PIN):
+                if is_encoding:
+                    print("Recording in progress")
+                    if GPIO.input(HILIGHT_PIN):
+                        if READY_FOR_HILIGHT:
+                            print("send hilight event")
+                            tag_hilight()
+                            READY_FOR_HILIGHT = False
+                    else:
+                        READY_FOR_HILIGHT = True
                 else:
-                    READY_FOR_HILIGHT = True
+                    print("send start recording")
+                    start_recording()
             else:
-                print("send start recording")
-                IS_RECORDING = True
-        else:
-            if IS_RECORDING:
-                print("send stop recording")
-                IS_RECORDING = False
-                READY_FOR_HILIGHT = True
-            
-        sleep(0.5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+                if is_encoding:
+                    print("send stop recording")
+                    stop_recording()
+                    READY_FOR_HILIGHT = True
+
+            print(".")
+            sleep(0.5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        gopro.close()
+
+if __name__ == "__main__":
+    sys.exit(main())
